9b4038ef8a0c044f6c41bfc9adc40c6add4222a8
vromano <vrrpizzato@gmail.com>
2021-07-16 11:57:10 -0300

    coverage context tests

--- test/blog/posts/post_test.exs
+++ test/blog/posts/post_test.exs
@@ -2,20 +2,50 @@ defmodule Blog.PostsTest do
   use Blog.DataCase
   alias Blog.{Posts, Posts.Post}
 
-
-  describe "Testes de criação de post" do
-
+  describe "Testes de funcionalidades de Contexto do Post" do
     @valid_post %{
       title: "Phoenix Framework",
       description: "Lorem Ipsum"
     }
 
+    @update_post %{
+      title: "Framework Phoenix",
+      description: "Ipsum Lorem"
+    }
+
+    def post_fixture(attrs \\ %{}) do
+      {:ok, post} = Posts.create_post(@valid_post)
+      post
+    end
+
     test "create_post" do
-      assert {:ok, %Post{} = post} = Posts.create_post(@valid_post)
+      post = post_fixture()
       assert post.title == "Phoenix Framework"
       assert post.description == "Lorem Ipsum"
     end
 
-  end
+    test "list_posts" do
+      post = post_fixture()
+      assert Posts.list_posts() == [post]
+    end
 
+    test "get_post" do
+      post = post_fixture()
+      assert Posts.get_post!(post.id) == post
+    end
+
+    test "update_post" do
+      post = post_fixture()
+      assert {:ok, %Post{} = post} = Posts.update_post(post.id, @update_post)
+      assert post.title == "Framework Phoenix"
+    end
+
+    test "delete_post" do
+      assert post = Posts.delete_post(post_fixture().id)
+
+      assert_raise Ecto.NoResultsError, fn ->
+        Posts.get_post!(post.id)
+      end
+    end
+  end
 end
